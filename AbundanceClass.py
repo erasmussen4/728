0914e414030fcece3d8677084860cebb83fdb3c8
erasmussen4 <rasmussen.emma.e@gmail.com>
2024-12-07 20:31:39 -0700

    More updates

--- AbundanceClass.py
+++ AbundanceClass.py
@@ -1,11 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import interp1d
+from scipy.special import zeta, roots_laguerre
+from scipy.integrate import solve_ivp, quad
+
 class Abundance():
-
-    import matplotlib.pyplot as plt
-    import numpy as np
-    from scipy.interpolate import interp1d
-    from scipy.special import zeta, roots_laguerre
-    from scipy.integrate import solve_ivp, quad
-
     def __init__(self, T0=1e1, T1=1e-2, N_nu=3):
         #Constants
         self.node = roots_laguerre(64)[0]
@@ -15,9 +14,11 @@ class Abundance():
         self.A = {"n": 1, "p": 1, "2H": 2, "3H": 3, "3He": 3, "4He": 4, "7Li": 7, "7Be": 7}
         self.B = {"n": 0, "p": 0, "2H": 2.22, "3H": 8.48, "3He": 7.72, "4He": 28.3, "7Li": 39.25, "7Be": 37.6}
         self.gA = {"n": 2, "p": 2, "2H": 3, "3H": 2, "3He": 2, "4He": 1, "7Li": 4, "7Be": 4}
+        self.Z = {"n": 0, "p": 1, "2H": 1, "3H": 1, "3He": 2, "4He": 2, "7Li": 3, "7Be": 4}
         self.h = 1.15e-5
         self.H0 = 1
         self.me = 0.510998911
+        self.mn = 939.565346    # {MeV}, neutron mass
         self.G = 6.70881e-45
         self.hbar = 6.58211899e-22
         self.k_B = 8.617343e-11 #MeV/K
@@ -43,7 +44,7 @@ class Abundance():
         self.int_rates_a = np.array([
             [2.5e4, 0, 0, 0, 1, 0, 0, 0, 0, 0],
             [2.23e3, 0, 1, 0.112, 3.38, 2.65, 0, 0, 0, -3.72],
-            [1, 0, 0, 0, 1, 75.5, 0, 0, 1250, 0, 0],
+            [1, 0, 0, 0, 75.5, 0, 0, 1250, 0, 0],
             [7.06e8, 0, 0, 0, 1, 0, 0, 0, 0, 0],
             [2.87e4, 0, 1, 0.108, 0.466, 0.352, 0.3, 0.576, 0, -3.87],
             [6e3, 0, 0, 0, 1, 0, 0, 0, 0, 0],
@@ -87,8 +88,8 @@ class Abundance():
     
     def expansion(self,T0,T1,nstep=256):
         T = np.geomspace(T0,T1,nstep)
-        soln_t = solve_ivp(self.dt_dT,T[[0,-1]],[0],t_eval=T)
-        soln_Tnu = solve_ivp(self.dTnu_dT,T[[0,-1]],[T0],t_eval=T)
+        soln_t = solve_ivp(self.dt_dT,T[[0,-1]],[0],"LSODA",t_eval=T)
+        soln_Tnu = solve_ivp(self.dTnu_dT,T[[0,-1]],[T0],"LSODA",t_eval=T)
         print(len(soln_t.t), len(soln_Tnu.y[0]), len(soln_t.y[0]))
         return soln_t.t, soln_Tnu.y[0], soln_t.y[0]
     
@@ -165,15 +166,18 @@ class Abundance():
         """ 
         return self.dr_dT(T,r=0)/(3*self.H(T)) #changed, used to be just self.H
     
-    def X_eq(self, elemKey, T):
+    def X_eq(self, elemKey, T, X_n, X_p):
         """
-            Calculates dr/dT for any given temperature.
+            Calculates X_eq for any given temperature.
         """
-        A = self.A[elemKey]
-        zeta = zeta(3)
-        g = self.gA[elemKey]
+        A = self.A[elemKey]     # {}, atomic mass number
+        zeta, eta = self.zeta3, self.eta
+        g = self.gA[elemKey]    # {}, number of spins
+        Z = self.Z[elemKey]     # {}, charge (# of protons)
+        B = self.B[elemKey]     # {}, binding energy
         prefactor = g * zeta**(A-1) * 2**((3*A-5)/2) * np.pi**((1-A)/2) * A**(5/2)
-        return 
+        T_depend = (T/self.mn)**(3*(A-1)/2) * np.exp(B/T)
+        return prefactor * T_depend * eta**(A-1) * X_p**Z * X_n**(A-Z) 
     
     def rho_b(self, T):
         """
@@ -219,31 +223,84 @@ class Abundance():
         T9 = T/(self.k_B*1e9)
         return a * rho_b**(b) * T9**c * np.exp(d*T9**(-1)) * self.interaction_rates_a(reactionIdx, T)
     
+    def calc_reaction_rate(self, T, X_A):
+        """
+            Calculate the reaction rates from the rate equations.
+
+        """
+        dX0_dt = X_A[2]*self.interaction_rates_b(0,T) + X_A[3]*self.interaction_rates_b(2,T) + X_A[3]*X_A[1]*self.interaction_rates_b(3,T) + X_A[5]*self.interaction_rates_b(5,T) +.5*X_A[2]**2*self.interaction_rates_a(6,T) + X_A[2]*X_A[3]*self.interaction_rates_a(10,T) + X_A[3]**2*self.interaction_rates_a(12,T) + X_A[3]*X_A[4]*self.interaction_rates_a(13,T) + X_A[6]*X_A[1]*self.interaction_rates_b(17,T) + .5*X_A[5]**2*self.interaction_rates_b(19,T) - X_A[0]*(X_A[1]*self.interaction_rates_a(0,T) + X_A[2]*self.interaction_rates_a(2,T) + X_A[4]*self.interaction_rates_a(3,T) + X_A[4]*self.interaction_rates_a(5,T) + X_A[4]*self.interaction_rates_b(6,T) + X_A[5]*self.interaction_rates_b(10,T) + X_A[0]*X_A[5]*self.interaction_rates_b(12,T) + X_A[1]*X_A[5]*self.interaction_rates_b(13,T) + X_A[7]*self.interaction_rates_a(17,T) + X_A[7]*self.interaction_rates_a(19,T))
+        dX1_dt = X_A[2]*self.interaction_rates_b(0,T) + X_A[4]*self.interaction_rates_b(1,T) + X_A[0]*X_A[4]*self.interaction_rates_a(3,T) + X_A[5]*self.interaction_rates_b(4,T) + .5*X_A[2]*self.interaction_rates_a(7,T) + X_A[2]*X_A[4]*self.interaction_rates_a(9,T) + .5*X_A[4]**2*self.interaction_rates_a(11,T) + X_A[4]*X_A[3]*self.interaction_rates_a(13,T) + X_A[0]*X_A[6]*self.interaction_rates_a(17,T) + .5*X_A[5]**2*self.interaction_rates_b(18,T) - X_A[1]*(X_A[0]*self.interaction_rates_a(0,T) + X_A[2]*self.interaction_rates_a(1,T) + X_A[3]*self.interaction_rates_b(3,T) + X_A[3]*self.interaction_rates_a(4,T) + X_A[3]*self.interaction_rates_b(7,T) + X_A[5]*self.interaction_rates_b(9,T) + .5*X_A[5]*X_A[1]*self.interaction_rates_b(11,T) + X_A[5]*X_A[0]*self.interaction_rates_b(13,T) + X_A[6]*self.interaction_rates_b(17,T) + X_A[6]*self.interaction_rates_a(18,T))
+        dX2_dt = X_A[1]*X_A[0]*self.interaction_rates_a(0,T) + X_A[4]*self.interaction_rates_b(1,T) + X_A[3]*self.interaction_rates_b(2,T) + 2*X_A[4]*self.interaction_rates_b(6,T) + 2*X_A[3]*self.interaction_rates_b(7,T) + X_A[5]*(2*self.interaction_rates_b(8,T) +self.interaction_rates_b(9,T) + self.interaction_rates_b(10,T)) + X_A[4]*X_A[3]*self.interaction_rates_a(14,T) - X_A[2]*(self.interaction_rates_b(0,T) + X_A[1]*self.interaction_rates_a(1,T) + X_A[0]*self.interaction_rates_a(2,T) + X_A[2]*(self.interaction_rates_a(6,T) + self.interaction_rates_a(7,T) + self.interaction_rates_a(8,T)) + X_A[4]*self.interaction_rates_a(9,T) + X_A[3]*self.interaction_rates_a(10,T) + X_A[5]*self.interaction_rates_b(14,T)) 
+        dX3_dt = X_A[0]*X_A[2]*self.interaction_rates_a(2,T) + X_A[0]*X_A[4]*self.interaction_rates_a(3,T) + X_A[5]*self.interaction_rates_b(4,T) + .5*X_A[2]**2*self.interaction_rates_a(7,T) + X_A[5]*X_A[0]*(self.interaction_rates_b(10,T) + X_A[0]*self.interaction_rates_b(12,T) + X_A[1]*self.interaction_rates_b(13,T)) + X_A[5]*X_A[2]*self.interaction_rates_b(14,T) + X_A[6]*self.interaction_rates_b(16,T) - X_A[3]*(self.interaction_rates_b(2,T) + X_A[1]*(self.interaction_rates_b(3,T) + self.interaction_rates_a(4,T) + self.interaction_rates_b(7,T)) + X_A[2]*self.interaction_rates_a(10,T) + X_A[3]*self.interaction_rates_a(12,T) + X_A[4]*(self.interaction_rates_a(13,T) + self.interaction_rates_a(14,T)) + X_A[5]*self.interaction_rates_a(16,T))
+        dX4_dt = X_A[1]*X_A[2]*self.interaction_rates_a(1,T) + X_A[1]*X_A[3]*self.interaction_rates_b(3,T) + X_A[5]*self.interaction_rates_b(5,T) + .5*X_A[2]**2*self.interaction_rates_a(6,T) + X_A[5]*X_A[1]*self.interaction_rates_b(9,T) + X_A[5]*X_A[1]**2*self.interaction_rates_b(11,T) + X_A[5]*X_A[1]*X_A[0]*self.interaction_rates_b(13,T) + X_A[5]*X_A[2]*self.interaction_rates_b(14,T) * X_A[7]*self.interaction_rates_b(15,T) - X_A[4]*(self.interaction_rates_b(1,T) + X_A[0]*(self.interaction_rates_a(3,T) + self.interaction_rates_a(5,T) + self.interaction_rates_b(6,T)) + X_A[2]*self.interaction_rates_a(9,T) + X_A[4]*self.interaction_rates_a(11,T) + X_A[3]*(self.interaction_rates_a(13,T) + self.interaction_rates_a(14,T)) + X_A[5]*self.interaction_rates_a(15,T))
+        dX5_dt = X_A[1]*X_A[3]*self.interaction_rates_a(4,T) + X_A[0]*X_A[4]*self.interaction_rates_a(5,T) + .5*X_A[2]**2*self.interaction_rates_a(8,T) + X_A[2]*X_A[4]*self.interaction_rates_a(9,T) + X_A[2]*X_A[3]*self.interaction_rates_a(10,T) + .5*X_A[4]**2*self.interaction_rates_a(11,T) + .5*X_A[3]**2*self.interaction_rates_a(12,T) + X_A[4]*X_A[3]*(self.interaction_rates_a(13,T) + self.interaction_rates_a(14,T)) + X_A[7]*self.interaction_rates_b(15,T) + X_A[6]*self.interaction_rates_b(16,T) + 2*X_A[1]*X_A[6]*self.interaction_rates_a(18,T) + 2*X_A[0]*X_A[7]*self.interaction_rates_a(19,T) - X_A[5]*(self.interaction_rates_b(4,T) + self.interaction_rates_b(5,T) + self.interaction_rates_b(8,T) + X_A[1]*self.interaction_rates_b(9,T) + X_A[0]*self.interaction_rates_b(10,T) + X_A[1]**2*self.interaction_rates_b(11,T) + X_A[0]**2*self.interaction_rates_b(12,T) + X_A[1]*X_A[0]*self.interaction_rates_b(13,T) + X_A[2]*self.interaction_rates_b(14,T) + X_A[4]*self.interaction_rates_a(15,T) + X_A[3]*self.interaction_rates_a(16,T) + X_A[5]*(self.interaction_rates_b(18,T) + self.interaction_rates_b(19,T)))
+        dX6_dt = X_A[3]*X_A[5]*self.interaction_rates_a(16,T) + X_A[0]*X_A[7]*self.interaction_rates_a(17,T) + .5*X_A[5]**2*self.interaction_rates_b(18,T) - X_A[6]*(self.interaction_rates_b(16,T) + X_A[1]*self.interaction_rates_b(17,T) + X_A[1]*self.interaction_rates_a(18,T))
+        dX7_dt = X_A[4]*X_A[5]*self.interaction_rates_a(15,T) + X_A[1]*X_A[6]*self.interaction_rates_b(17,T) + .5*X_A[5]**2*self.interaction_rates_b(19,T) - X_A[7]*(self.interaction_rates_b(15,T) + X_A[0]*(self.interaction_rates_a(17,T) + self.interaction_rates_a(18,T)))
+        dX_dt = np.array([dX0_dt, dX1_dt, dX2_dt, dX3_dt, dX4_dt, dX5_dt, dX6_dt, dX7_dt])
+        return dX_dt
+        
     def dXA_dt(self, T, X_A):
         """
-            Calculate the derivatives from the rate equations.
-
+            Calculate the derivatives from the rate equations, using equilibrium
+            abundances when Hubble rate dominates reaction rates.
         """
-        dX0_dt = self.X_A[2]*self.interaction_rates_b(0,T) + self.X_A[4]*self.interaction_rates_b(1,T) + self.X_A[1]*self.X_A[4]*self.interaction_rates_a(3,T) + self.X_A[5]*self.interaction_rates_b(4,T) + .5*self.X_A[2]*self.interaction_rates_a(7,T) + self.X_A[2]*self.X_A[4]*self.interaction_rates_a(9,T) + .5*self.X_A[4]**2*self.interaction_rates_a(11,T) + self.X_A[4]*self.X_A[3]*self.interaction_rates_a(13,T) + self.X_A[1]*self.X_A[6]*self.interaction_rates_a(17,T) + .5*self.X_A[5]**2*self.interaction_rates_b(18,T) - self.X_A[0]*(self.X_A[1]*self.interaction_rates_a(0,T) + self.X_A[2]*self.interaction_rates_a(1,T) + self.X_A[3]*self.interaction_rates_b(3,T) + self.X_A[3]*self.interaction_rates_a(4,T) + self.X_A[3]*self.interaction_rates_b(7,T) + self.X_A[5]*self.interaction_rates_b(9,T) + .5*self.X_A[5]*self.X_A[0]*self.interaction_rates_b(11,T) + self.X_A[5]*self.X_A[1]*self.interaction_rates_b(13,T) + self.X_A[6]*self.interaction_rates_b(17,T) + self.X_A[6]*self.interaction_rates_a(18,T))
-        dX1_dt = self.X_A[2]*self.interaction_rates_b(0,T) + self.X_A[3]*self.interaction_rates_b(2,T) + self.X_A[3]*self.X_A[0]*self.interaction_rates_b(3,T) + self.X_A[5]*self.interaction_rates_b(5,T) +.5*self.X_A[2]**2*self.interaction_rates_a(6,T) + self.X_A[2]*self.X_A[3]*self.interaction_rates_a(10,T) + self.X_A[3]**2*self.interaction_rates_a(12,T) + self.X_A[3]*self.X_A[4]*self.interaction_rates_a(13,T) + self.X_A[6]*self.X_A[0]*self.interaction_rates_b(17,T) + .5*self.X_A[5]**2*self.interaction_rates_b(19,T) - self.X_A[1]*(self.X_A[0]*self.interaction_rates_a(0,T) + self.X_A[2]*self.interaction_rates_a(2,T) + self.X_A[4]*self.interaction_rates_a(3,T) + self.X_A[4]*self.interaction_rates_a(5,T) + self.X_A[4]*self.interaction_rates_b(6,T) + self.X_A[5]*self.interaction_rates_b(10,T) + self.X_A[1]*self.X_A[5]*self.interaction_rates_b(12,T) + self.X_A[0]*self.X_A[5]*self.interaction_rates_b(13,T) + self.X_A[7]*self.interaction_rates_a(17,T) + self.X_A[7]*self.interaction_rates_a(19,T))
-        dX2_dt = self.X_A[0]*self.X_A[1]*self.interaction_rates_a(0,T) + self.X_A[4]*self.interaction_rates_b(1,T) + self.X_A[3]*self.interaction_rates_b(2,T) + 2*self.X_A[4]*self.interaction_rates_b(6,T) + 2*self.X_A[3]*self.interaction_rates_b(7,T) + self.X_A[5]*(2*self.interaction_rates_b(8,T) +self.interaction_rates_b(9,T) + self.interaction_rates_b(10,T)) + self.X_A[4]*self.X_A[3]*self.interaction_rates_a(14,T) - self.X_A[2]*(self.interaction_rates_b(0,T) + self.X_A[0]*self.interaction_rates_a(1,T) + self.X_A[1]*self.interaction_rates_a(2,T) + self.X_A[2]*(self.interaction_rates_a(6,T) + self.interaction_rates_a(7,T) + self.interaction_rates_a(8,T)) + self.X_A[4]*self.interaction_rates_a(9,T) + self.X_A[3]*self.interaction_rates_a(10,T) + self.X_A[5]*self.interaction_rates_b(14,T)) 
-        dX3_dt = self.X_A[1]*self.X_A[2]*self.interaction_rates_a(2,T) + self.X_A[1]*self.X_A[4]*self.interaction_rates_a(3,T) + self.X_A[5]*self.interaction_rates_b(4,T) + .5*self.X_A[2]**2*self.interaction_rates_a(7,T) + self.X_A[5]*self.X_A[1]*(self.interaction_rates_b(10,T) + self.X_A[1]*self.interaction_rates_b(12,T) + self.X_A[0]*self.interaction_rates_b(13,T)) + self.X_A[5]*self.X_A[2]*self.interaction_rates_b(14,T) + self.X_A[6]*self.interaction_rates_b(16,T) - self.X_A[3]*(self.interaction_rates_b(2,T) + self.X_A[0]*(self.interaction_rates_b(3,T) + self.interaction_rates_a(4,T) + self.interaction_rates_b(7,T)) + self.X_A[2]*self.interaction_rates_a(10,T) + self.X_A[3]*self.interaction_rates_a(12,T) + self.X_A[4]*(self.interaction_rates_a(13,T) + self.interaction_rates_a(14,T)) + self.X_A[5]*self.interaction_rates_a(16,T))
-        dX4_dt = self.X_A[0]*self.X_A[2]*self.interaction_rates_a(1,T) + self.X_A[0]*self.X_A[3]*self.interaction_rates_b(3,T) + self.X_A[5]*self.interaction_rates_b(5,T) + .5*self.X_A[2]**2*self.interaction_rates_a(6,T) + self.X_A[5]*self.X_A[0]*self.interaction_rates_b(9,T) + self.X_A[5]*self.X_A[0]**2*self.interaction_rates_b(11,T) + self.X_A[5]*self.X_A[0]*self.X_A[1]*self.interaction_rates_b(13,T) + self.X_A[5]*self.X_A[2]*self.interaction_rates_b(14,T) * self.X_A[7]*self.interaction_rates_b(15,T) - self.X_A[4]*(self.interaction_rates_b(1,T) + self.X_A[1]*(self.interaction_rates_a(3,T) + self.interaction_rates_a(5,T) + self.interaction_rates_b(6,T)) + self.X_A[2]*self.interaction_rates_a(9,T) + self.X_A[4]*self.interaction_rates_a(11,T) + self.X_A[3]*(self.interaction_rates_a(13,T) + self.interaction_rates_a(14,T)) + self.X_A[5]*self.interaction_rates_a(15,T))
-        dX5_dt = self.X_A[0]*self.X_A[3]*self.interaction_rates_a(4,T) + self.X_A[1]*self.X_A[4]*self.interaction_rates_a(5,T) + .5*self.X_A[2]**2*self.interaction_rates_a(8,T) + self.X_A[2]*self.X_A[4]*self.interaction_rates_a(9,T) + self.X_A[2]*self.X_A[3]*self.interaction_rates_a(10,T) + .5*self.X_A[4]**2*self.interaction_rates_a(11,T) + .5*self.X_A[3]**2*self.interaction_rates_a(12,T) + self.X_A[4]*self.X_A[3]*(self.interaction_rates_a(13,T) + self.interaction_rates_a(14,T)) + self.X_A[7]*self.interaction_rates_b(15,T) + self.X_A[6]*self.interaction_rates_b(16,T) + 2*self.X_A[0]*self.X_A[6]*self.interaction_rates_a(18,T) + 2*self.X_A[1]*self.X_A[7]*self.interaction_rates_a(19,T) - self.X_A[5]*(self.interaction_rates_b(4,T) + self.interaction_rates_b(5,T) + self.interaction_rates_b(8,T) + self.X_A[0]*self.interaction_rates_b(9,T) + self.X_A[1]*self.interaction_rates_b(10,T) + self.X_A[0]**2*self.interaction_rates_b(11,T) + self.X_A[1]**2*self.interaction_rates_b(12,T) + self.X_A[0]*self.X_A[1]*self.interaction_rates_b(13,T) + self.X_A[2]*self.interaction_rates_b(14,T) + self.X_A[4]*self.interaction_rates_a(15,T) + self.X_A[3]*self.interaction_rates_a(16,T) + self.X_A[5]*(self.interaction_rates_b(18,T) + self.interaction_rates_b(19,T)))
-        dX6_dt = self.X_A[3]*self.X_A[5]*self.interaction_rates_a(16,T) + self.X_A[1]*self.X_A[7]*self.interaction_rates_a(17,T) + .5*self.X_A[5]**2*self.interaction_rates_b(18,T) - self.X_A[6]*(self.interaction_rates_b(16,T) + self.X_A[0]*self.interaction_rates_b(17,T) + self.X_A[0]*self.interaction_rates_a(18,T))
-        dX7_dt = self.X_A[4]*self.X_A[5]*self.interaction_rates_a(15,T) + self.X_A[0]*self.X_A[6]*self.interaction_rates_b(17,T) + .5*self.X_A[5]**2*self.interaction_rates_b(19,T) - self.X_A[7]*(self.interaction_rates_b(15,T) + self.X_A[1]*(self.interaction_rates_a(17,T) + self.interaction_rates_a(18,T))) 
+        #dX_dt = np.zeros_like(X_A)  # To store time derivatives
+        
+        # Compute Hubble rate
+        hubble_rate = self.H(T)
+        reaction_rates = self.calc_reaction_rate(T, X_A)
+        
+        # Loop over elements
+        for idx, elem in self.elem.items():
+            # Compute reaction rate for this element
+            reaction_rate = reaction_rates[idx]
+            
+            # Compare with Hubble rate
+            if reaction_rate < hubble_rate:
+                if idx > 1:
+                    # Use equilibrium abundance
+                    X_A[idx] = self.X_eq(elem, T, X_A[0], X_A[1])
+    #        else:
+    #            # Compute full rate equation
+    #            # Replace this with the actual rate calculation for your element
+    #            if idx == 0:  # Example for neutrons
+    #                dX_dt[idx] = (X_A[1] * self.interaction_rates_a(0, T)
+    #                              - X_A[0] * self.interaction_rates_b(0, T))
+    #            elif idx == 1:  # Example for protons
+    #                dX_dt[idx] = (X_A[0] * self.interaction_rates_b(0, T)
+    #                              - X_A[1] * self.interaction_rates_a(0, T))
+    #            # Extend this logic for other elements based on reactions
+    #            # involving `X_A[idx]`. 
 
-        return np.array([dX0_dt, dX1_dt, dX2_dt, dX3_dt, dX4_dt, dX5_dt, dX6_dt, dX7_dt])
+        dX_dt = np.array(self.calc_reaction_rate(T, X_A))
+        return dX_dt
 
-    def BBN(self, N=64, nsteps=256): 
-        t0,t1 = np.interp([self.T0,self.T1],self.T[::-1],self.time[::-1])
-        t = np.geomspace(t0,t1,nsteps)
-        self.X_A = np.zeros(N)
+    def BBN(self, nsteps=256):
+        t0, t1 = np.interp([self.T0, self.T1], self.T[::-1], self.time[::-1])
+        t = np.geomspace(t0, t1, nsteps)
 
-        # Set the neutron and proton initial conditions
-        self.X_A[0] = 1/(np.exp(self.Q_np/self.T0)+1)
-        self.X_A[1] = 1-self.X_A[0]
+        # Set the initial conditions
+        self.X_A = np.zeros(len(self.elem))  # Match size to number of elements
+        self.X_A[1] = 1 / (np.exp(self.Q_np / self.T0) + 1) # neutron I.C.
+        self.X_A[0] = 1 - self.X_A[1]                       # proton I.C.
 
-        soln = solve_ivp(self.dXA_dt,t[[0,-1]],self.X_A,'LSODA',t_eval=t)
-        X = (soln.y.T * self.A).T[[self.elem.index(i) for i in len(self.elem)]]
-        return np.interp(t,self.time,self.T), X
+        # Solve ODEs
+        soln = solve_ivp(self.dXA_dt, [t0, t1], self.X_A, method='LSODA', t_eval=t)
+
+        # Convert results using atomic masses
+        A_values = np.array([self.A[key] for key in self.elem.values()])
+        print(soln.y)
+        X = (soln.y.T * A_values).T
+        return np.interp(t, self.time, self.T), X
+
+# Run the code
+testing = Abundance()
+T_vals, X = testing.BBN()
+plt.axis([2, 1e-2, 1e-13, 2])
+plt.loglog(T_vals, X.T)
+plt.legend([testing.elem[i] for i in range(8)])
+plt.xlabel('T = temperature / MeV')
+plt.ylabel('X = mass fraction')
+plt.ylim(1e-12, 1e0)
+print(X[0])
+plt.show()
